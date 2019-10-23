873ed172ab002896d10311237b09a49570b88ece
Beato Bongco <beatobongco@gmail.com>
2019-10-23 11:49:18 +0800

    Bump to 0.0.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="TFeatureExtractor",
-    version="0.0.7",
+    version="0.0.8",
     author="Beato Bongco",
     author_email="beatobongco@gmail.com",
     description="Vectorize strings in 2 lines of code with the latest Transformer models",
